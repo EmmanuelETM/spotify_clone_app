9a519c11e3410a9f5e09afcd406b0724ed2c3484
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-11-25 15:52:40 -0400

    Feat: Add background image, spotify logo and text to get-started page

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_clone_app/core/configs/assets/app_images.dart';
+import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone_app/core/configs/theme/app_colors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBG)
+              )
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset(
+                    AppVectors.logo
+                  ),
+                ),
+                const Spacer(),
+                const Text(
+                  'Enjoy Listening to Music',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                const Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color : AppColors.grey,
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            )
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+        ],
+      ),
+    );
+  }
+}
