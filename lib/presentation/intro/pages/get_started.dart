1b3fdeb1607024a954ee498fef1b2089961be87b
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-11-25 19:01:10 -0400

    Refactor: Add choose_theme page, and refactor the get-started page so it redirects correctly to the choose_theme page

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -4,6 +4,7 @@ import 'package:spotify_clone_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_images.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
 import 'package:spotify_clone_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_clone_app/presentation/choose_theme/pages/choose_theme.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -23,46 +24,57 @@ class GetStartedPage extends StatelessWidget {
                 image: AssetImage(AppImages.introBG)
               )
             ),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: SvgPicture.asset(
-                    AppVectors.logo
-                  ),
-                ),
-                const Spacer(),
-                const Text(
-                  'Enjoy Listening to Music',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(height: 20,),
-                const Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercise',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color : AppColors.grey,
-                    fontSize: 13,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                BasicAppButton(
-                  onPressed: () {
-
-                  }, 
-                  title: 'Get started',
-                )
-              ],
-            )
           ),
           Container(
             color: Colors.black.withOpacity(0.15),
           ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 50,
+              horizontal: 40
+            ),
+            child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      AppVectors.logo
+                    ),
+                  ),
+                  const Spacer(),
+                  const Text(
+                    'Enjoy Listening to Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercise',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color : AppColors.grey,
+                      fontSize: 13,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  BasicAppButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const ChooseThemePage(),
+                        ),
+                      );
+                    }, 
+                    title: 'Get started',
+                  )
+                ],
+              ),
+          )
         ],
       ),
     );
