3d23fcac740456bd92dc84ab776daf4370fae3f5
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-11-25 20:36:12 -0400

    Feat: Implemented logic for changing themes

--- lib/presentation/choose_theme/pages/choose_theme.dart
+++ lib/presentation/choose_theme/pages/choose_theme.dart
@@ -1,10 +1,12 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify_clone_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_images.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
 import 'package:spotify_clone_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_clone_app/presentation/choose_theme/bloc/theme_cubit.dart';
 
 class ChooseThemePage extends StatelessWidget {
   const ChooseThemePage({super.key});
@@ -57,22 +59,27 @@ class ChooseThemePage extends StatelessWidget {
                     children: [
                       Column(
                         children: [
-                          ClipOval(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(
-                                sigmaX: 10,
-                                sigmaY: 10,
-                                ),
-                              child: Container(
-                                height: 80,
-                                width: 80,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: const Color(0xff30393C).withOpacity(0.5),
-                                ),
-                                child: SvgPicture.asset(
-                                  AppVectors.moon,
-                                  fit: BoxFit.none
+                          GestureDetector(
+                            onTap: () {
+                              context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                            },
+                            child: ClipOval(
+                              child: BackdropFilter(
+                                filter: ImageFilter.blur(
+                                  sigmaX: 10,
+                                  sigmaY: 10,
+                                  ),
+                                child: Container(
+                                  height: 80,
+                                  width: 80,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: const Color(0xff30393C).withOpacity(0.5),
+                                  ),
+                                  child: SvgPicture.asset(
+                                    AppVectors.moon,
+                                    fit: BoxFit.none
+                                  ),
                                 ),
                               ),
                             ),
@@ -91,22 +98,27 @@ class ChooseThemePage extends StatelessWidget {
                       const SizedBox(width: 80,),
                       Column(
                         children: [
-                          ClipOval(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(
-                                sigmaX: 10,
-                                sigmaY: 10,
-                                ),
-                              child: Container(
-                                height: 80,
-                                width: 80,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: const Color(0xff30393C).withOpacity(0.5),
-                                ),
-                                child: SvgPicture.asset(
-                                  AppVectors.sun,
-                                  fit: BoxFit.none
+                          GestureDetector(
+                            onTap: () {
+                              context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                            },
+                            child: ClipOval(
+                              child: BackdropFilter(
+                                filter: ImageFilter.blur(
+                                  sigmaX: 10,
+                                  sigmaY: 10,
+                                  ),
+                                child: Container(
+                                  height: 80,
+                                  width: 80,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: const Color(0xff30393C).withOpacity(0.5),
+                                  ),
+                                  child: SvgPicture.asset(
+                                    AppVectors.sun,
+                                    fit: BoxFit.none
+                                  ),
                                 ),
                               ),
                             ),
