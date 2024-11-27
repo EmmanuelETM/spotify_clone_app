4067f3c6dc60cce19f8f8f84440f359a6d96e2a6
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-11-26 22:59:16 -0400

    Feat: Finished sign_up page's ui, working on sign_in

--- lib/presentation/auth/pages/sign_in.dart
+++ lib/presentation/auth/pages/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify_clone_app/common/widgets/appbar/app_bar.dart';
+import 'package:spotify_clone_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
 
 class SignInPage extends StatelessWidget {
@@ -9,16 +10,100 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: _signupText(context),
       appBar: BasicAppBar(
         title: SvgPicture.asset(
-          AppVectors.logo
+          AppVectors.logo,
+          height: 40,
+          width: 40,
         )
       ),
-      body: const Column(
-        children: [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 50, 
+          horizontal: 30
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _signinText(),
+            const SizedBox(height: 50),
+            _emailField(context),
+            const SizedBox(height: 20),
+            _passwordField(context),
+            const SizedBox(height: 20),
+            BasicAppButton(
+              onPressed: () {
 
-        ],
+              }, 
+              title: 'Create Account'
+            ),
+           const SizedBox(height: 20),
+          ],
+        ),
       )
     );
   }
+
+  Widget _signinText() {
+    return const Text(
+      'Sign In',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(
+        hintText: 'Enter email',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      )
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(
+        hintText: 'Password',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      )
+    );
+  }
+
+  Widget _signupText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 30 
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Not a Memeber yet?',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14
+            ),
+          ),
+          TextButton(
+            onPressed: (){}, 
+            child: const Text(
+              'Create an Account',
+              style: TextStyle(
+                color: Color(0xff1db954),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
+
+// 1:46:42
--- lib/presentation/auth/pages/sign_up.dart
+++ lib/presentation/auth/pages/sign_up.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:spotify_clone_app/common/widgets/appbar/app_bar.dart';
+import 'package:spotify_clone_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_clone_app/presentation/auth/pages/sign_in.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
@@ -9,6 +11,7 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: _signinText(context),
       appBar: BasicAppBar(
         title: SvgPicture.asset(
           AppVectors.logo,
@@ -16,11 +19,31 @@ class SignUpPage extends StatelessWidget {
           width: 40,
         )
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          _registerText(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 50, 
+          horizontal: 30
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _registerText(),
+            const SizedBox(height: 50),
+            _fullNameField(context),
+            const SizedBox(height: 20),
+            _emailField(context),
+            const SizedBox(height: 20),
+            _passwordField(context),
+            const SizedBox(height: 20),
+            BasicAppButton(
+              onPressed: () {
+
+              }, 
+              title: 'Create Account'
+            ),
+           const SizedBox(height: 20),
+          ],
+        ),
       )
     );
   }
@@ -36,5 +59,72 @@ class SignUpPage extends StatelessWidget {
     );
   }
 
+  Widget _fullNameField(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(
+        hintText: 'Full Name',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      )
+      
+    );
+  }
+
+    Widget _emailField(BuildContext context) {
+      return TextField(
+        decoration: const InputDecoration(
+          hintText: 'Enter email',
+        ).applyDefaults(
+          Theme.of(context).inputDecorationTheme,
+        )
+      );
+    }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      decoration: const InputDecoration(
+        hintText: 'Password',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      )
+    );
+  }
+
+  Widget _signinText(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 30 
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Do you have an Account?',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => const SignInPage()
+                ),
+              );
+            },
+            child: const Text(
+              'Sign In',
+              style: TextStyle(
+                color: Color(0xff1db954),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
+// 1:46:42
